1b831844204c1d97d929f25ba7d305eb84138b2a
rbartru <rbartru@uw.edu>
2019-03-06 15:24:50 -0800

    fixed bug in misclassification error

--- pytorch_warmup.py
+++ pytorch_warmup.py
@@ -9,7 +9,7 @@ import random
 matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
 with gzip.open("mnist.pkl.gz") as f:
-        train_set, valid_set, test_set = pickle.load(f, encoding="bytes")
+		train_set, valid_set, test_set = pickle.load(f, encoding="bytes")
 
 # Initialize dataset
 X = train_set[0]
@@ -39,29 +39,44 @@ b = torch.zeros(10, requires_grad=True)
 
 def model(X):
 	return X @ W + b
+
+def compute_yhat(X):
+	w = W.detach().numpy()
+	B = b.detach().numpy()
+	return X @ w + B
+
 loss_fn = torch.nn.CrossEntropyLoss()
 
 optim = torch.optim.SGD([W, b], lr = 0.1)
 
 def misclass_err(X,Y,y_hat,w,b):
-    Yhat_labels = np.argmax(y_hat, axis=0)
-    errors = (Y == Yhat_labels)
-    return 100 * np.sum(errors)/(y_hat.shape[0]*1.0)
+	#print(np.shape(y_hat))
+	Yhat_labels = np.argmax(y_hat, axis=1)
+	errors = np.sum(Y != Yhat_labels)
+	#print(errors)
+	#print(y_hat.shape[0])
+	return 100 * errors/(y_hat.shape[0]*1.0)
 
 
 iterations = []
 train_loss = []
 dev_loss = []
 mis_class_err = []
-for i in range(1):
-	y_hat = model(X_torch)
-	#loss = loss_fn(y_hat, Y_torch)
+min_error = 100
+for i in range(10000):
+	y_hat_np = compute_yhat(X_test)
+	miss_err = misclass_err(X_test,Y_test,y_hat_np,W,b)
+	if (miss_err < min_error):
+		min_error = miss_err
+
+	y_hat_torch = model(X_torch)
+	loss = loss_fn(y_hat_torch, Y_torch)
 	#y_hat_dev = model(X_torch_dev)
 	#loss_dev = loss_fn(y_hat_dev, Y_torch_dev)
-	miss_err = misclass_err(X_test,Y_test,y_hat,W,b)
+	
 
-	if i % 20 == 0:
-		print('i', i, 'loss', miss_err)
+	if i % 500 == 0 and i > 200:
+		print('i', i, 'loss', loss.item(), 'miss err', miss_err)
 		iterations.append(i)
 		#train_loss.append(loss)
 		#dev_loss.append(loss_dev)
@@ -71,13 +86,14 @@ for i in range(1):
 	loss.backward()
 	optim.step()
 
+print(min_error)
+
 
 #plt.plot(iterations, train_loss, label='train log loss')
-plt.plot(iterations, mis_class_err, label='dev')
+plt.plot(iterations, mis_class_err, label='test')
 plt.legend(bbox_to_anchor=(0.4,0.9),loc=2,borderaxespad=0.)
 plt.xlabel('iterations')
-plt.ylabel('Log loss error')
-plt.title('Train and Dev set log loss')
+plt.ylabel('misclassification error')
+plt.title('Test set misclassification error')
 plt.show()
-
 
